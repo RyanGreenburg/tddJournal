b4f666c44ae8d4c4b533c47e4abd62ca68f62897
RYAN GREENBURG <ryangreenburg@RYANs-MacBook-Pro-2.local>
2019-09-09 09:27:58 -0600

    initial commit

---
+++ tddJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tddJournal
+//
+//  Created by RYAN GREENBURG on 9/9/19.
+//  Copyright © 2019 RYAN GREENBURG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
